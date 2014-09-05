24edc418e46466cb3cb25219696026cb1d917788
robbiesh <sherrard.rob@gmail.com>
2014-09-05 15:12:57 -0400

    newsletter page

---
+++ page-2231.php
@@ -0,0 +1,48 @@
+<?php get_header();
+if ( have_posts() ) { while ( have_posts() ) { the_post(); ?>
+<div id="modal">
+  <div id="modal-box">
+    <div class="gutter">
+      <div id="modalClose">X</div>
+      <div id="modal-content">
+        <?php dynamic_sidebar('modal-box'); ?>
+      </div>
+    </div>
+  </div>
+  <div id="modal-overlay">
+
+  </div>
+</div>
+
+<div id="content" class="<?php the_field('color'); ?>-theme">
+  <div class="container cf">
+    <a href="<?php the_field('left_cta_link'); ?>">
+      <div id="welcome-callout">
+        <div class="gutter">
+          <?php the_field('left_cta'); ?>
+        </div>
+      </div>
+    </a>
+    <div id="contentPrimary">
+      <div class="gutter cf">
+        <div class="titleCont">
+          <h1><?php the_title(); ?></h1>
+        </div>
+        <?php if(has_post_thumbnail()){
+            echo "<div id='page-banner'>".the_post_thumbnail()."</div>";
+          }
+        ?>
+        <?php the_content(); ?>
+      </div>
+    </div>
+
+    <div id="contentSecondary">
+      <div class="gutter">
+        <?php get_template_part( 'partials/page', 'sidebar' ); ?>
+      </div>
+    </div>
+  </div>
+  <div class="colorbar"></div>
+</div>
+<?php } } ?>
+<?php get_footer(); ?>
